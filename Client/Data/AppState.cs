8576b8cf71a44cfbcca18e59edb1de5d39b989b9
Tyrease Teer <kayaneseera@gmail.com>
2023-04-01 22:08:55 -0700

    PWA Initialized

---
+++ Client/Data/AppState.cs
@@ -0,0 +1,19 @@
+﻿namespace OsuryuhaanDictionaryBlazorApp.Client.Data
+{
+    public class AppState
+    {
+        public AppState()
+        {
+            CurrentViewName= "Default";
+        }
+
+        public string? CurrentViewName { get; private set; } = "Default";
+        public event Action? StateChanged;
+        private void NotifyStateChanged() => StateChanged?.Invoke();
+        public void SetName(ReadOnlySpan<char> newName)
+        {
+            CurrentViewName= newName.ToString();
+            NotifyStateChanged();
+        }
+    }
+}
